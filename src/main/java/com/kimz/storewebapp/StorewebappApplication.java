76b7073531beb7707ebe1ae16d502a75e6d78783
Kimutai <kimutai0003@gmail.com>
2021-05-05 05:31:42 +0300

    init changes

---
+++ src/main/java/com/kimz/storewebapp/StorewebappApplication.java
@@ -0,0 +1,13 @@
+package com.kimz.storewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorewebappApplication.class, args);
+	}
+
+}
